c81d52b26b3e9e990a093ee47b7c499519f4a61a
Trev <trevordavies99@gmail.com>
2021-03-29 01:34:31 -0400

    More refine

--- script.js
+++ script.js
@@ -8,20 +8,16 @@ function upload()
     userInput = document.getElementById("user").value;
     if (userInput > 0){
       pos++;
-      alert("Positive");
     }
     else if (userInput < 0){
       neg ++;
-      alert("Negative");
     }
     else if (userInput == 0){
-      alert("Zero");
       results();
     }
 }
 function results()
 {
-  alert("Results Called");
   document.getElementById("positive").textContent= pos;
   document.getElementById("negative").textContent= neg;
 }
